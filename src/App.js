c106cf789d935041d590da212a84de7f3d88c0ea
Tesfaye Belachew Abebe <get.tesfaye.belachew@gmail.com>
2020-04-22 15:57:33 +0300

    renamed home page file and updated app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Home from "./components/pages/Home";
+import Home from "./components/pages/home";
 // import VisibleSidebar from "./components/pages/Sidebar";
 import Users from "./components/pages/Users";
 import Topics from "./components/pages/Topics";
