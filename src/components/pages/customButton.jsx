01e31f37d4432f9e59e3dec579f4cc6ec4e5b029
Tesfaye Belachew Abebe <get.tesfaye.belachew@gmail.com>
2020-04-22 13:50:45 +0300

    removed uncessary files

--- src/components/pages/customButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./custom-button.styles.scss";
-function customButton({ children, ...otherProps }) {
-  return <button className="custom-button">{children}</button>;
-}
-
-export default customButton;
